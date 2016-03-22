2350a6aea7451f5caa776305a4f190f22c31e0f4
Ted Dobyns <ted.ajax@gmail.com>
2016-03-21 18:10:30 -0700

    sadsf

--- ShootHard/main.cpp
+++ ShootHard/main.cpp
@@ -48,6 +48,17 @@ int run()
     input::init();
     texture::manager::init(foundation::memory_globals::default_allocator(), renderer);
 
+    static const float32 vertexData[] = {
+        -1.f, -1.f, 0.f,
+        1.f, -1.f, 0.f,
+        0.f, 1.f, 0.f,
+    };
+
+    uint vertexBuffer;
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData, GL_STATIC_DRAW);
+
     bool isRunning = true;
 
     while (isRunning) {
@@ -72,8 +83,14 @@ int run()
             isRunning = false;
         }
 
-        glClearColor(1.f, 0.f, 0.f, 1.f);
+        glClearColor(0.f, 1.f, 0.f, 1.f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDisableVertexAttribArray(0);
 
         SDL_GL_SwapWindow(window);
 
